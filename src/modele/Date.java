b46088286b18a5996382d28ab570c7304510b780
Reinald Raphat <reinald.raphat@edu.univ-fcomte.fr>
2016-10-12 03:04:35 +0200

    added Hotel attribute to Room

---
+++ src/modele/Date.java
@@ -0,0 +1,27 @@
+package modele;
+
+/**
+ * Created by Fox Mc-Tag on 12/10/2016.
+ * Property of Mc-Tag's corporation, all rights reserved.
+ */
+public class Date {
+
+    String data;
+    //TO DO: seek and implement necessary attributes to control database validity of data
+
+
+
+    public Date(String data){this.data= data;}
+
+
+
+
+    public Date get(){return this;}
+    public String getData(){return data;}
+
+    public void setData(String data){this.data= data;}
+
+
+
+
+}
