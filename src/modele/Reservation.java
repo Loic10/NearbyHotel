8d64579a1f2b5b3ac95cb081234fcdef01fdfcdd
Reinald Raphat <reinald.raphat@edu.univ-fcomte.fr>
2016-10-12 03:18:19 +0200

    implemented Reservation: attributes constructors getters setters

--- src/modele/Reservation.java
+++ src/modele/Reservation.java
@@ -1,5 +1,8 @@
 package modele;
 
+import modele.hotel.Room;
+import modele.user.User;
+
 /**
  * Created by Fox Mc-Tag on 05/10/2016.
  * Property of Mc-Tag's corporation, all rights reserved.
@@ -7,5 +10,39 @@ package modele;
 public class Reservation {
 
     private Int ID;
+    private Date arrivalDate;
+    private Date departureDate;
+    private User user;
+    private Room room;
+
+
+
+    public Reservation(Int ID, Date arrivalDate, Date departureDate, User user, Room room){
+        this.ID= ID;
+        new Reservation(arrivalDate, departureDate, user, room);
+    }
+    public Reservation(Date arrivalDate, Date departureDate, User user, Room room){
+        this.arrivalDate= arrivalDate;
+        this.departureDate= departureDate;
+        this.user= user;
+        this.room= room;
+    }
+
+
+
+
+    public Reservation get(){return this;}
+    public Int getID(){return ID;}
+    public Date getArrivalDate(){return arrivalDate;}
+    public Date getDepartureDate(){return departureDate;}
+    public Room getRoom(){return room;}
+
+    public void setID(Int ID){this.ID= ID;}
+    public void setArrivalDate(Date arrivalDate){this.arrivalDate= arrivalDate;}
+    public void setDepartureDate(Date departureDate){this.departureDate= departureDate;}
+    public void setRoom(Room room){this.room= room;}
+
+
+
 
 }
