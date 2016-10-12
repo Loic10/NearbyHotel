8c5aece6948e23b76647a3eba7eb4817ade6d8cc
Reinald Raphat <reinald.raphat@edu.univ-fcomte.fr>
2016-10-12 02:46:06 +0200

    implemented Ville  with:        attributes        constructores        getters        setters

--- src/modele/Ville.java
+++ src/modele/Ville.java
@@ -5,4 +5,29 @@ package modele;
  * Property of Mc-Tag's corporation, all rights reserved.
  */
 public class Ville {
+
+    private Int ID;
+    private Varchar name;
+
+
+
+    public Ville(Int ID, Varchar name){
+        this.ID= ID;
+        new Ville(name);
+    }
+    public Ville(Varchar name){this.name= name;}
+
+
+
+
+    public Ville get(){return this;}
+    public Int getID(){return ID;}
+    public Varchar getName(){return name;}
+
+    public void setID(Int ID){this.ID= ID;}
+    public void setName(Varchar name){this.name= name;}
+
+
+
+
 }
